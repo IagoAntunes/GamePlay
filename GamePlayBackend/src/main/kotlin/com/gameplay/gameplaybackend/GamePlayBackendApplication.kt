1368f9cdd992edbda9ec81de822831028c4f52b2
IagoAntunes <iagoantunes.f@gmail.com>
2024-07-06 11:47:00 -0300

    first commit

---
+++ GamePlayBackend/src/main/kotlin/com/gameplay/gameplaybackend/GamePlayBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gameplay.gameplaybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamePlayBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GamePlayBackendApplication>(*args)
+}
---
+++ GamePlayBackend/src/test/kotlin/com/gameplay/gameplaybackend/GamePlayBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gameplay.gameplaybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamePlayBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
