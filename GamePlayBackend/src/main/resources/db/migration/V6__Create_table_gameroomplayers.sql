e76bccb2bff28c602d0444e9f32c4e020adc5110
IagoAntunes <iagoantunes.f@gmail.com>
2024-09-11 22:02:05 -0300

    feat: Docker-Compose

--- GamePlayBackend/src/main/resources/db/migration/V6__Create_table_gameroomplayers.sql
+++ GamePlayBackend/src/main/resources/db/migration/V6__Create_table_gameroomplayers.sql
@@ -3,5 +3,5 @@ CREATE TABLE gameroomplayers (
           id_user BIGINT NOT NULL,
           id_game_room BIGINT NOT NULL,
       CONSTRAINT fk_gameRoomPlayer_user FOREIGN KEY (id_user) REFERENCES users(id),
-      CONSTRAINT fk_gameRoomPlayer_gameRoom FOREIGN KEY (id_game_room) REFERENCES gameRoom(id)
+      CONSTRAINT fk_gameRoomPlayer_gameRoom FOREIGN KEY (id_game_room) REFERENCES gameroom(id)
 );
